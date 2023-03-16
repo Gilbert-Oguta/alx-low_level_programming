4bb3ea562500181761988dfb9bd7400ffcec3670
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-16 14:53:26 -0700

    Hello,puts4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 
-	char Mystr[] = "Programming is like building a multilingual puzzle";
-
-	puts(Mystr);
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return(0);
 }
