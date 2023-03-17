12bf1b07a945ff22a2978e55a5e810a726d4e8fa
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-17 15:15:22 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s = 'a';
+
+	while (s <= 'z')
+	{
+		putchar(s);
+		s++;
+	}
+
+	s = 'A';
+
+	while (s <= 'Z')
+	{
+		putchar(s);
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+	
