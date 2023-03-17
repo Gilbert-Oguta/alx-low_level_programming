5070f64a339955c957cc0a53e4240810f087acd2
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-17 15:43:11 -0700

    alphabet except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s = 'a';
+
+	while (s <= 'z')
+	{
+		if (s != 'e' && s != 'q')
+			putchar(s);
+		s++
+	}
+	putchar('\n');
+
+	return (0);
+}
