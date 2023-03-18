5a890ec8a8840479a0752dbcbf4aea98ce2760a3
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-17 17:23:14 -0700

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 90; a++)
+	{
+		for ()
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+		}
+		if (a != 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
