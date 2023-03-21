daeda7cface1ec2010be6f62622380d37de207d5
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-21 14:52:08 -0700

    alphabet_lowercase10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet, lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char j;
+
+	while (i++ <= 9)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('/n');
+	}
+}
