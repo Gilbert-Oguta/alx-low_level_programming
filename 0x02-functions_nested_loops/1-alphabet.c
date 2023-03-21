cecc1de95fde79d0e15ebc37f92bf5e0b4ac4d29
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-21 14:31:59 -0700

    alphabet_lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase from a-z
+ * Return: Nothing
+ */
+
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+		_putchar(k);
+	
+	_putchar('\n');
+}
