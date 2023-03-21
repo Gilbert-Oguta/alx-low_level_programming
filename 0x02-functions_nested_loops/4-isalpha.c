e0b00b458f2599d0cfabec717370bc40856e1baf
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-21 15:31:22 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,9 +5,10 @@
  * Return: 1 if char is alphabet lower or upper, 0 otherwise
  */
 
-int _isalpha(intc)
+int _isalpha(int c)
+{
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	return (1);
+		return (1);
 	else
-	return (0);
+		return (0);
 }
