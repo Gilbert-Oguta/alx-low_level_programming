a61d0db5196c50c43fd2d1e7cb9f5ef33b1d4aaf
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-21 13:36:04 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 /**
  * main - Entry point
@@ -9,13 +7,14 @@
 
 int main(void)
 {
-	char c[] = "_putchar";
-	int d;
-
-	for (d = 0; d < 9; d++)
-	{
-		_putchar(c[d]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
