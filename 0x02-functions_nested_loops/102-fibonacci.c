72e6bb80c9daf5d64f9afb1f9d2ba355198aa101
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-22 14:57:51 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * fibonacci -  prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * Return: nothing
+ */
+int main(void)
+{
+	unsigned long count, a, b, c;
+
+	a = 0;
+	b = 1;
+	for (count = 0; count < 50; count++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%lu", c);
+		if (count = 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
