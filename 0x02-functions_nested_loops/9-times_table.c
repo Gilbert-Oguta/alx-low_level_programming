07c8eb1c28a70fef95f7440a223f99f33c6335b7
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-21 17:21:07 -0700

    time_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int a, b, table;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <=9; b++)
+		{
+			table = (a * b);
+			if (b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (table >= 10)
+			{
+				_putchar((table / 10) + '0');
+				_putchar((table % 10) + '0');
+			}
+			else if (table < 10 && b != 0)
+			{
+				_putchar(' ');
+				_putchar((table % 10) + '0');
+			}
+			else
+				_putchar((table % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
