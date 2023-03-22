386067448f1626787c113666bdf8218429f28c2f
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-22 16:37:51 -0700

    sum_even

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -  finds and prints the sum of the even-valued terms
+ * Return: return 0
+ */
+int main(void)
+{
+	unsigned long j = 0, k = 1, m = 0, sum = 0;
+	while (m <= 4000000)
+	{
+		m = j + k;
+		j = k;
+		k = m;
+
+		if ((j % 2) == 0)
+		sum += j;
+
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
