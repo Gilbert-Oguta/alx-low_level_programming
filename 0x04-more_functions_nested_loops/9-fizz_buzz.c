75666064f3df1fc52c2dbdf464e397c3676facf8
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-23 12:37:05 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,11 +10,11 @@ int main(void)
 
 	for (y = 1; y <= 100; y++)
 	{
-		if ((y % 3 == 0) && ( y % 5 == 0))
+		if ((y % 3 == 0) && (y % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
-		else if (y % 3 ==0)
+		else if (y % 3 == 0)
 		{
 			printf("Fizz");
 		}
