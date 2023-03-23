73798adb181ff19f865cecea2fc33da57eb5a228
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-23 14:25:05 -0700

    prints_integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: The integer to print
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		x = n;
+		_putchar('_');
+	}
+	x /= 10;
+	if (x != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
+}
