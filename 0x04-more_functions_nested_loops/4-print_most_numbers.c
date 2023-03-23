deb352c03b98cfd2f1b281dc8f37a11810455196
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-23 08:27:06 -0700

    the numbers, from 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
  * print_most_numbers -  prints the numbers, from 0 to 9 except 2 and 4
  * Return: The numbers requested
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int a = 0;
 
