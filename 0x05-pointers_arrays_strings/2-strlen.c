ddb5a85077f75370da624a48f6b2eb3e06c4c8a8
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-27 08:08:46 -0700

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,12 @@
 /**
  * _strlen -  returns the length of a string
  * @s - string
- * Return: none
+ * Return: string length
  */
 int _strlen(char *s)
 {
 	int num, incr;
+
 	incr = 0;
 	for (num = 0; s[num] != '\0'; num++)
 		incr++;
