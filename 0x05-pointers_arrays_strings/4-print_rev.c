5c40f4f45e285a64957ce73948c9b3c1c352cc0f
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-27 09:09:25 -0700

    string reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,10 +6,10 @@
  */
 void print_rev(char *s)
 {
-	int leng = 0, index = 0;
-	leng = _strlen(s);
+	int len = 0, index = 0;
+	len = _strlen(s);
 
-	for (index = leng - 1; index >= 0; index--)
+	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
 
 	_putchar('\n');
