b976ae0edda69c9baa10e2fab2a3fc79da93c3e7
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-27 11:11:07 -0700

    character in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string, starting with the first character, followed by a new line.
+ * @str: string to be printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(s[a]);
+		}
+	}
+	_putchar('\n');
+}
