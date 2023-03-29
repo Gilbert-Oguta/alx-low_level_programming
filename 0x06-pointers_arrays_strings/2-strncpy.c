0412fe2ffa189650aedd5eec0aeb09e6b5a1249d
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-03-29 09:22:01 -0700

    copies string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies a string, with number of bites
+ * @dest: buffer storing the string copy
+ * @src: the source string
+ * @n: max number of bytes copied
+ * Return: copied string with the number of bytes
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; x < n && src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	for (; x < n; x++)
+	{
+		dest[x] = '\0';
+	}
+	return (dest);
+}
