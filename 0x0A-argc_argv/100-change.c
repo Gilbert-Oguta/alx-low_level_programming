8000e1e13d769f5e0887912ee4221637c3f44521
Gilbert-Oguta <gilbertoguta37@gmail.com>
2023-04-15 18:53:19 -0700

    number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,36 +10,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int amnt, a, chng;
+	int cents, ycoins = 0;
 
-	int coins[] = {25, 10, 5, 2, 10};
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-			if (argc != 2)
-			{
-				printf("Error\n");
+	cents = atoi(argv[1]);
 
-				return (1);
-			}
-
-			amnt = atoi(argv[1]);
-			chng = 0;
-
-			if (amnt < 0)
-			{
-				printf("0\n");
-				return (0);
-			}
-
-			for (a = 0; a < 5 && amnt > 0; a++)
-			{
-				while (amnt >= coins[a])
-				{
-					chng++;
-
-					amnt = coins[a];
-				}
-			}
-
-			printf("%d\n", chng);
-			return (0);
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ycoins += 1;
+	}
+	printf("%d\n", ycoins);
+	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * atoi - program that multiplies two numbers
+ * main - program that multiplies two numbers
  * @argc: number of particular arguments
  * @argv: array that contains particular arguments
  * Return: Success 0
  */
-int _atoi(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int f1 = 0, f2 = 0;
-
-	if (argc == 3)
-	{
-	f1 = _atoi(argv[1]);
-	f2 = _atoi(argv[2]);
-	printf("%d\n", f1 * f2);
-	}
-	else 
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
