355aa06a487199f94ff09bae92c9065e7e082d37
Gilbert Oguta <gilbertogutaa37@gmail.com>
2023-05-01 00:46:32 +0000

    listing

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,18 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef LISTS_FILE
+#define LISTS_FILE
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
